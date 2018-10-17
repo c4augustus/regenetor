e2386fc53984fb214cb2fec6f9d611b2775bcf93
Christopher Augustus <repos@c4augustus.com>
2018-10-17 23:29:59 +0200

    develop mutation of the original AST by randomly selecting a single leaf node within it--without actually mutating it yet

--- regenetor.py
+++ regenetor.py
@@ -1,7 +1,27 @@
 #!/usr/local/bin/python3
 
 import ast
+import random
 import sys
+
+def make_mutant_ast(originalAst):
+    mutantAst = originalAst
+    mutate_random_leaf_ast_node(mutantAst)
+    return mutantAst
+
+def mutate_random_leaf_ast_node(astNode):
+    childCount = 0
+    for index, childNode in enumerate(ast.iter_child_nodes(astNode)):
+        childCount += 1
+    if childCount == 0: # astNode is a leaf
+        print("selected random leaf AST node: {}"
+              .format(ast.dump(astNode)))
+        ### TODO: MUTATE astNode
+    else: # randomly select child node and recurse into it
+        randIndex = random.randrange(0, childCount)
+        for index, childNode in enumerate(ast.iter_child_nodes(astNode)):
+            if index == randIndex:
+                mutate_random_leaf_ast_node(childNode)
 
 def parse_itself():
     filename = sys.argv[0]
@@ -12,7 +32,9 @@ def parse_itself():
 
 def main():
     print("regenetor commence")
-    print("AST >>>>>>\n{}\n<<<<<< AST".format(ast.dump(parse_itself())))
+    originalAst = parse_itself()
+    print("AST >>>>>>\n{}\n<<<<<< AST".format(ast.dump(originalAst)))
+    mutatedAst = make_mutant_ast(originalAst)
     print("regenetor complete")
 
 if __name__ == '__main__':
